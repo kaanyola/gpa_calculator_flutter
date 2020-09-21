ba2620d2b50433ac7ddf9e089f0ce4d30ab97efa
kaanyola <kaanyola@gmail.com>
2020-09-22 01:45:19 +0300

    class screen created

--- lib/screens/class/class_screen.dart
+++ lib/screens/class/class_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gpa_calculator_flutter/screens/class/components/body.dart';
+import 'package:gpa_calculator_flutter/screens/result/result_screen.dart';
+import 'package:gpa_calculator_flutter/size_config.dart';
 
 class ClassScreen extends StatefulWidget {
   @override
@@ -8,16 +11,45 @@ class ClassScreen extends StatefulWidget {
 class _ClassScreenState extends State<ClassScreen> {
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(Icons.arrow_back_ios),
-          color: Colors.black,
-        ),
+      appBar: buildAppBar(context),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      leading: IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: Icon(Icons.arrow_back_ios),
+        color: Colors.black,
       ),
+      actions: [
+        Padding(
+          padding: EdgeInsets.only(right: 8, top: 4, bottom: 4),
+          child: MaterialButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ResultScreen()),
+              );
+            },
+            color: Theme.of(context).primaryColor,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+            child: Text("Calculate",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    letterSpacing: 2)),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/screens/class/components/body.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:gpa_calculator_flutter/size_config.dart';
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  List _gradesAA = ["AA", "BA", "BB", "CB", "CC", "DC", "DD", "FD", "FF"];
+
+  List<DropdownMenuItem<String>> _dropDownMenuItems;
+  String _currentGrade;
+
+  List<DropdownMenuItem<String>> getDropDownMenuItems() {
+    List<DropdownMenuItem<String>> items = new List();
+    for (String grade in _gradesAA) {
+      // here we are creating the drop down menu items, you can customize the item right here
+      // but I'll just use a simple text for this
+      items.add(new DropdownMenuItem(value: grade, child: new Text(grade)));
+    }
+    return items;
+  }
+
+  void changedDropDownItem(String selectedGrade) {
+    print("Selected city $selectedGrade, we are going to refresh the UI");
+    setState(() {
+      _currentGrade = selectedGrade;
+    });
+  }
+
+  void initState() {
+    _dropDownMenuItems = getDropDownMenuItems();
+    _currentGrade = _dropDownMenuItems[0].value;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    double defaultSize = SizeConfig.defaultSize;
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return buildClassCard(width, height, context);
+      },
+    );
+  }
+
+  Padding buildClassCard(double width, double height, BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(width * 0.05),
+      child: Container(
+        padding: EdgeInsets.all(width * 0.025),
+        width: width * 0.4,
+        height: SizeConfig.defaultSize * 22,
+        decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 3,
+                offset: Offset(0, 3), // changes position of shadow
+              ),
+            ],
+            borderRadius: BorderRadius.circular(4),
+            border: Border.all(color: Theme.of(context).primaryColor)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                    prefixText: "1.",
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    prefixStyle: Theme.of(context)
+                        .textTheme
+                        .bodyText2
+                        .copyWith(fontSize: 20),
+                    hintText: "Class name"),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Grade:"),
+                  SizedBox(
+                    width: width * 0.05,
+                  ),
+                  DropdownButton(
+                    value: _currentGrade,
+                    items: _dropDownMenuItems,
+                    onChanged: changedDropDownItem,
+                  ),
+                ],
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: "Input your credit"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
