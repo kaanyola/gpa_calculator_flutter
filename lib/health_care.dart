5e7e6dfbda55974001c8849a6adfe75534105ed3
kaanyola <kaanyola@gmail.com>
2020-10-22 01:44:30 +0300

    added

---
+++ lib/health_care.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HealthcareView extends StatefulWidget {
+  @override
+  _HealthcareViewState createState() => _HealthcareViewState();
+}
+
+class _HealthcareViewState extends State<HealthcareView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
